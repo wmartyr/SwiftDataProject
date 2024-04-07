1cb62f482917201f82944c16cc9682bf6c7b2cab
Woodrow Martyr <woodrow.martyr@gmail.com>
2024-04-07 09:57:24 +0930

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Woodrow Martyr on 7/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
