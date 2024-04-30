366301f27f28a68316c29f08ec20f66331deaafa
Woodrow Martyr <woodrow.martyr@gmail.com>
2024-05-01 04:40:18 +0930

    Add relationships.

---
+++ SwiftDataProject/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  SwiftDataProject
+//
+//  Created by Woodrow Martyr on 7/4/2024.
+//
+
+import Foundation
---
+++ SwiftDataProject/UsersView.swift
@@ -0,0 +1,18 @@
+//
+//  UsersView.swift
+//  SwiftDataProject
+//
+//  Created by Woodrow Martyr on 12/4/2024.
+//
+
+import SwiftUI
+
+struct UsersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UsersView()
+}
